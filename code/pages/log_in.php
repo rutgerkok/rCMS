1c766df5cc9f9107cd15fe79b0b592c8e1706598
Rutger Kok <rutger.kok@live.com>
2013-06-26 21:36:05 +0200

    Add admin links when an admin logs in

--- code/pages/log_in.php
+++ code/pages/log_in.php
@@ -14,7 +14,17 @@ class LoginPage extends Page {
         return Authentication::$USER_RANK;
     }
 
-    public function get_page_content(Website $oWebsite) {       
+    public function get_page_content(Website $oWebsite) {
+        $admin_links = "";
+        if ($oWebsite->logged_in_staff(true)) {
+            $admin_links = <<<EOT
+                <br />
+                <a href="{$oWebsite->get_url_page("account_management")}" class="arrow">{$oWebsite->t("main.account_management")}</a>
+                <br />
+                <a href="{$oWebsite->get_url_page("admin")}" class="arrow">{$oWebsite->t("main.admin")}</a>
+EOT;
+        }
+
         return <<<EOT
             <h3>{$oWebsite->t('users.logged_in')}</h3>
             <p>{$oWebsite->t('users.succesfully_logged_in')}</p>
@@ -22,6 +32,7 @@ class LoginPage extends Page {
                 <a href="{$oWebsite->get_url_main()}" class="arrow">{$oWebsite->t("main.home")}</a>
                 <br />
                 <a href="{$oWebsite->get_url_page("account")}" class="arrow">{$oWebsite->t("main.my_account")}</a>
+                $admin_links
             </p>
 EOT;
     }
