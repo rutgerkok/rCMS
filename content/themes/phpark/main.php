f57f0b629ce9c71c8979c3b9072a5d2e4eed598b
rutgerkok <rutger.kok@live.com>
2013-10-30 12:24:48 +0100

    Start making PHPark theme responsive
    It works better on mobile phones now, but it is far from perfect.

--- content/themes/phpark/main.php
+++ content/themes/phpark/main.php
@@ -8,6 +8,7 @@ if (!defined("WEBSITE")) {
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, user-scalable=no">
         <link href="<?php echo $this->get_url_theme() . "main.css" ?>" rel="stylesheet" type="text/css" />
         <script src="<?php echo $this->getUrlJavaScripts() ?>tooltip.js"></script>
         <!--[if lte IE 8]>
@@ -17,64 +18,62 @@ if (!defined("WEBSITE")) {
     </head>
     <body>
         <div id="container">
-            <div id="containerwrapper">
-                <div id="header">
-                    <h1>
+            <div id="header">
+                <h1>
 <?php echo $this->getSiteTitle(); ?>
-                    </h1>
-                    <div id="search">
+                </h1>
+                <div id="search">
 <?php $this->echo_search_form(); ?>
-                    </div>
-                        <?php if ($this->isLoggedIn()) { ?>
-                        <div id="account_label">
-                        <?php $this->echo_account_label(); ?>
-                            <div id="account_box">
-                            <?php $this->echo_account_box(80); ?>
-                                <div style="clear:both"></div>
-                            </div>
+                </div>
+                    <?php if ($this->isLoggedIn()) { ?>
+                    <div id="account_label">
+                    <?php $this->echo_account_label(); ?>
+                        <div id="account_box">
+                        <?php $this->echo_account_box(80); ?>
+                            <div style="clear:both"></div>
                         </div>
+                    </div>
 <?php } ?>
-                </div>
-                <div id="hornav">
-                    <ul>
+            </div> <!-- id="header" -->
+            <div id="hornav">
+                <ul>
 <?php $this->echo_menu(); ?>
+                </ul>
+                    <?php if (!$this->isLoggedIn()) { ?>
+                    <ul id="accountlinks">
+                    <?php $this->echo_accounts_menu(); ?>
                     </ul>
-                        <?php if (!$this->isLoggedIn()) { ?>
-                        <ul id="accountlinks">
-                        <?php $this->echo_accounts_menu(); ?>
-                        </ul>
-                        <?php } ?>
-                </div>
-                <div <?php
-                        if ($this->getPageType() == "HOME") {
-                            echo 'id="content"';
-                        } elseif ($this->getPageType() == "BACKSTAGE") {
-                            echo 'id="contentadmin"';
-                        } else { // dus $this->get_page_type()=="NORMAL"
-                            echo 'id="contentwide"';
-                        }
-                        ?> >
-                    <!-- Einde header -->
+                    <?php } ?>
+            </div> <!-- id="hornav" -->
+            <div <?php
+                    if ($this->getPageType() == "HOME") {
+                        echo 'id="content"';
+                    } elseif ($this->getPageType() == "BACKSTAGE") {
+                        echo 'id="contentadmin"';
+                    } else { // dus $this->get_page_type()=="NORMAL"
+                        echo 'id="contentwide"';
+                    }
+                    ?> >
+                <!-- Einde header -->
 
 <?php $this->echoPageContent(); ?>
 
-                    <!-- Begin footer -->
+                <!-- Begin footer -->
 
-                </div><!-- id="content"/"contentwide" -->
+            </div><!-- id="content"/"contentwide" -->
 
 <?php if ($this->getPageType() == "HOME") { ?>
-                    <div id="sidebar">
-                    <?php $this->echoWidgets(2); ?>
-                    </div>
-                    <div id="nav">
-    <?php $this->echoWidgets(3); ?>
-                    </div>
-                    <?php } ?>
-
-                <div id="footer">
-<?php $this->echo_copyright(); ?>
+                <div id="sidebar">
+                <?php $this->echoWidgets(2); ?>
                 </div>
-            </div><!-- id="containerwrapper" -->
+                <div id="nav">
+<?php $this->echoWidgets(3); ?>
+                </div>
+                <?php } ?>
+
+            <div id="footer">
+<?php $this->echo_copyright(); ?>
+            </div>
         </div><!-- id="container" -->
     </body>
 </html>	
