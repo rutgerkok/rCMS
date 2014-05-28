00691f5cc075295ef58e30fda4bf8b58d24f8881
rutgerkok <rutger.kok@live.com>
2014-05-28 10:51:04 +0200

    Check returned passwords
    Fix for versions where $2y$ is not available, but CRYPT_BLOWFISH is.
    
    Now also checks the return values, and throws an exception if everything
    fails.

--- src/Core/HashHelper.php
+++ src/Core/HashHelper.php
@@ -5,23 +5,46 @@ namespace Rcms\Core;
 class HashHelper {
 
     /**
+     * As noted in the PHP documentation, values shorter than this number of
+     * characters indicate a hashing failure.
+     */
+    const CRYPT_RETURN_VALUE_MIN_LENGHT = 13;
+
+    /**
      * Hashes the string using blowfish or md5, depending on what this server
      * supports. The hash is salted.
      * @param string $string The string to hash.
      */
     public static function hash($string) {
         if (CRYPT_BLOWFISH) {
-            // Blowfish, we're safe
-            $salt = '$2y$11$' . substr(md5(uniqid(rand(), true)), 0, 22);
-            return crypt($string, $salt);
-        } elseif (CRYPT_MD5) {
-            // Salted md5, let's hope for the best
-            $salt = '$1$' . substr(md5(uniqid(rand(), true)), 0, 8) . '$';
-            return crypt($string, $salt);
-        } else {
-            // There's no hope for this server anymore
-            return crypt($string);
+            // Blowfish
+            if (version_compare(PHP_VERSION, "5.3.7", '>')) {
+                $salt = '$2y$11$' . substr(md5(uniqid(rand(), true)), 0, 22);
+            } else {
+                $salt = '$2a$11$' . substr(md5(uniqid(rand(), true)), 0, 22);
+            }
+            $hashed = crypt($string, $salt);
+            if (strLen($hashed) >= self::CRYPT_RETURN_VALUE_MIN_LENGHT) {
+                return $hashed;
+            }
         }
+        if (CRYPT_MD5) {
+            // Salted md5
+            $salt = '$1$' . substr(md5(uniqid(rand(), true)), 0, 8) . '$';
+            $hashed = crypt($string, $salt);
+            if (strLen($hashed) >= self::CRYPT_RETURN_VALUE_MIN_LENGHT) {
+                return $hashed;
+            }
+        }
+
+        // Try the default algorithm
+        $hashed = crypt($string);
+        if (stLen($hashed) >= self::CRYPT_RETURN_VALUE_MIN_LENGHT) {
+            return $hashed;
+        }
+
+        // Failure
+        throw new Exception("Hashing of string failed");
     }
 
     /**
