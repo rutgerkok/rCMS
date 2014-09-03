rutgerkok/rCMS

commit 774a629857888d8cd056949c30185b7eb17f4816

Rewriting the database code
Now includes a simple query builder. Almost every page has to be
modified.

content/widgets/links/main.php
@@ -2,7 +2,7 @@
 
 namespace Rcms\Extend\Widget;
 
-use Rcms\Core\Menus;
+use Rcms\Core\LinkRepository;
 use Rcms\Core\Website;
 use Rcms\Core\WidgetDefinition;
 
@@ -15,7 +15,7 @@ class WidgetRkokLinks extends WidgetDefinition {
 
     const TITLE_MAX_LENGTH = 40;
 
-    public function getWidget(Website $oWebsite, $id, $data) {
+    public function getText(Website $oWebsite, $id, $data) {
         if (!isSet($data["menu_id"]) || !isSet($data["title"])) {
             return;
         }
@@ -30,7 +30,7 @@ class WidgetRkokLinks extends WidgetDefinition {
         }
 
         // Links
-        $oMenu = new Menus($oWebsite);
+        $oMenu = new LinkRepository($oWebsite);
         $returnValue.= '<ul class="linklist">';
         $returnValue.= $oMenu->getAsHtml($oMenu->getLinksByMenu($menu_id), true, $loggedInStaff);
         $returnValue.= "</ul>";
@@ -50,7 +50,7 @@ class WidgetRkokLinks extends WidgetDefinition {
         $returnValue = "";
         $title_max_length = self::TITLE_MAX_LENGTH; // Herodoc doesn't support constants
         // Build menu options
-        $oMenu = new Menus($oWebsite);
+        $oMenu = new LinkRepository($oWebsite);
         $menus = $oMenu->getMenus();
         $menu_options = "";
         if (count($menus) > 0) {
@@ -95,8 +95,8 @@ EOT;
             $data["valid"] = false;
         }
         $data["menu_id"] = isSet($_REQUEST["menu_id_" . $id]) ? (int) $_REQUEST["menu_id_" . $id] : 0;
-        $oMenu = new Menus($oWebsite);
-        if ($oMenu->getMenuByName($data["menu_id"]) == null) {
+        $oMenu = new LinkRepository($oWebsite);
+        if ($oMenu->getMenuName($data["menu_id"]) == null) {
             $oWebsite->addError($oWebsite->t("widgets.menu") . " " . $oWebsite->t("errors.not_found"));
             $data["valid"] = false;
         }

content/widgets/text/main.php
@@ -16,7 +16,7 @@ class WidgetRkokText extends WidgetDefinition {
      * (HTML) tags are saved unfiltered in the database, but filtered when displayed.
      */
 
-    public function getWidget(Website $oWebsite, $id, $data) {
+    public function getText(Website $oWebsite, $id, $data) {
         if (!isSet($data["text"]) || !isSet($data["title"])) {
             return "";
         }

src/Core/PlacedWidget.php
@@ -2,52 +2,39 @@
 
 namespace Rcms\Core;
 
-class PlacedWidget {
+use Rcms\Core\Repository\Entity;
 
-    private $id;
-    private $sidebarId;
-    private $dataString;
-    private $priority;
-    private $directoryName;
-    private $pathToDirectory;
+class PlacedWidget extends Entity {
+
+    protected $id;
+    protected $sidebarId;
+    protected $widgetData;
+    protected $priority;
+    protected $widgetName;
+    protected $baseDirectory;
 
     /**
      * Represents a widget. You'll need to fill out all parameters. The easiest
      * way to get this object is by calling {@link Widgets#get_placed_widget(int)}.
-     * 
-     * @param int $id The id of the widget. Set it to 0 to place a new widget.
-     * @param int $sidebarId The id of the sidebar of the widget.
-     * @param string $directoryName The name of the directory of the widget. 
-     *    Do not include the path.
-     * @param string $dataString JSON representation of the data of this widget. Can be null.
-     * @param int $priority Priority of the widget.
-     * @param string $directory The full directory of where this widget is
-     *    installed in.
+     *
+     * @param string $baseDirectory The base directory where all widgets are
+     * installed in.
      */
-    public function __construct($id, $sidebarId, $directoryName, $dataString,
-            $priority, $directory) {
-        $this->id = (int) $id;
-        $this->sidebarId = (int) $sidebarId;
-        $this->directoryName = $directoryName;
-        $this->dataString = $dataString;
-        if (!$this->dataString) {
-            $this->dataString = "{}";
-        }
-        $this->priority = (int) $priority;
-        $this->pathToDirectory = $directory;
+    public function __construct($baseDirectory) {
+        $this->baseDirectory = $baseDirectory;
     }
 
-    public function getWidgetDefinition(Widgets $widget_loader) {
-        return $widget_loader->getWidgetDefinition($this->directoryName);
+    public function getWidgetDefinition(WidgetRepository $widget_loader) {
+        return $widget_loader->getWidgetDefinition($this->baseDirectory . $this->widgetName . '/');
     }
 
     /**
      * Returns an array with key=>value pairs of data. Will never be null, but
      * can be empty.
-     * @return mixed The array.
+     * @return array The array.
      */
     public function getData() {
-        return JsonHelper::stringToArray($this->dataString);
+        return $this->widgetData;
     }
 
     /**
@@ -57,10 +44,10 @@ class PlacedWidget {
      */
     public function setData($data) {
         if ($data == null) {
-            $this->dataString = "{}";
+            $this->widgetData = array();
         } else {
             if (!isSet($data["valid"]) || $data["valid"]) {
-                $this->dataString = JsonHelper::arrayToString($data);
+                $this->widgetData = $data;
             }
         }
     }
@@ -95,7 +82,7 @@ class PlacedWidget {
      * @return WidgetInfoFile Info about the widget.
      */
     public function getWidgetInfo() {
-        return new WidgetInfoFile($this->directoryName, $this->pathToDirectory . "/info.txt");
+        return new WidgetInfoFile($this->widgetName, $this->baseDirectory . '/' . $this->widgetName . "/info.txt");
     }
 
     /**
@@ -103,7 +90,7 @@ class PlacedWidget {
      * @return string The name of the directory this widget is in.
      */
     public function getDirectoryName() {
-        return $this->directoryName;
+        return $this->widgetName;
     }
 
     /**

src/Core/WidgetRepository.php
@@ -1,0 +1,226 @@
+<?php
+
+namespace Rcms\Core;
+
+use Rcms\Core\Exception\NotFoundException;
+use Rcms\Core\Repository\Field;
+use Rcms\Core\Repository\Repository;
+
+/**
+ * Widget manager
+ */
+class WidgetRepository extends Repository {
+
+    const TABLE_NAME = "widgets";
+
+    // Reference to the Website.
+    private $websiteObject;
+    // Cache of all loaded widgets for this page
+    private $loadedWidgets = array();
+    // Temporary variable to store the directory name when echoeing the widgets
+    private $widgetDirectoryName;
+    private $sidebarIdField;
+    private $widgetDataField;
+    private $widgetIdField;
+    private $widgetNameField;
+    private $widgetPriorityField;
+
+    public function __construct(Website $oWebsite) {
+        parent::__construct($oWebsite->getDatabase());
+        $this->websiteObject = $oWebsite;
+
+        $this->sidebarIdField = new Field(Field::TYPE_INT, "sidebarId", "sidebar_id");
+        $this->widgetDataField = new Field(Field::TYPE_JSON, "widgetData", "widget_data");
+        $this->widgetIdField = new Field(Field::TYPE_PRIMARY_KEY, "id", "widget_id");
+        $this->widgetNameField = new Field(Field::TYPE_STRING, "widgetName", "widget_naam");
+        $this->widgetPriorityField = new Field(Field::TYPE_INT, "priority", "widget_priority");
+    }
+
+    public function createEmptyObject() {
+        return new PlacedWidget($this->websiteObject->getUriWidgets());
+    }
+
+    public function getTableName() {
+        return self::TABLE_NAME;
+    }
+
+    public function getPrimaryKey() {
+        return $this->widgetIdField;
+    }
+
+    public function getAllFields() {
+        return array($this->widgetIdField, $this->widgetDataField,
+            $this->widgetNameField, $this->widgetPriorityField,
+            $this->sidebarIdField);
+    }
+
+    /**
+     * Gets a list of all installed widgets.
+     * @return WidgetInfoFile[] List of all installed widgets.
+     */
+    public function getInstalledWidgets() {
+        $widgets = array();
+        $directoryToScan = $this->websiteObject->getUriWidgets();
+
+        // Check directory
+        if (!is_dir($directoryToScan)) {
+            return;
+        }
+
+        // Scan it
+        $files = scanDir($directoryToScan);
+        foreach ($files as $file) {
+            if ($file[0] != '.') {
+                // Ignore hidden files and directories above this one
+                if (is_dir($directoryToScan . $file)) {
+                    $widgets[] = new WidgetInfoFile($file, $directoryToScan . $file . "/info.txt");
+                }
+            }
+        }
+
+        return $widgets;
+    }
+
+    /**
+     * Shortcut to retrieve the widget areas. Also includes the home page as
+     * an option.
+     * @return array Array of widget, (numeric) id => name
+     */
+    public function getWidgetAreas() {
+        $areas = $this->websiteObject->getThemeManager()->getCurrentTheme()->getWidgetAreas($this->websiteObject);
+        $areas[1] = $this->websiteObject->t("widgets.homepage");
+        return $areas;
+    }
+
+    /**
+     * Returns a list of PlacedWidgets for the given sidebar.
+     * @param int $sidebar_id The id of the sidebar.
+     * @return PlacedWidget[] List of placed widgets.
+     */
+    public function getPlacedWidgetsFromSidebar($sidebar_id) {
+        return $this->where($this->sidebarIdField, '=', $sidebar_id)->orderDescending($this->widgetPriorityField)->select();
+    }
+
+    /**
+     * Searches the database for the widget with the given id.
+     * @param int $widget_id The id of the widget.
+     * @return PlacedWidget|null The placed widget, or null if it isn't found.
+     */
+    public function getPlacedWidget($widget_id) {
+        try {
+            return $this->where($this->getPrimaryKey(), '=', $widget_id)->selectOneOrFail();
+        } catch (NotFoundException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the widget the give directory.
+     * @param string $widgetDirectoryName Directory name. Do not include the full path.
+     * @return WidgetDefinition The widget, or null if not found.
+     */
+    public function getWidgetDefinition($widgetDirectoryName) {
+        if (!isSet($this->loadedWidgets[$widgetDirectoryName])) {
+            $this->widgetDirectoryName = $widgetDirectoryName;
+            $file = $this->websiteObject->getUriWidgets() . "/" . $widgetDirectoryName . "/main.php";
+            if (file_exists($file)) {
+                require($file);
+            }
+        }
+        if (isSet($this->loadedWidgets[$widgetDirectoryName])) {
+            return $this->loadedWidgets[$widgetDirectoryName];
+        } else {
+            return null;
+        }
+    }
+
+    // Echoes all widgets in the specified sidebar
+    public function getWidgetsHTML($sidebarId) {
+        $oWebsite = $this->websiteObject;
+        $oDB = $oWebsite->getDatabase();
+        if (!$oDB->isInstalled() || !$oDB->isUpToDate()) {
+            return $this->getSiteInstallError($sidebarId);
+        }
+        $loggedInAsAdmin = $oWebsite->isLoggedInAsStaff(true);
+        $output = "";
+
+        // Get all widgets that should be displayed
+        $result = $this->getPlacedWidgetsFromSidebar($sidebarId);
+
+        foreach ($result as $widget) {
+            $id = $widget->getId();
+            $directory_name = $widget->getDirectoryName();
+            $this->widgetDirectoryName = $directory_name;
+
+            // Try to load it if it isn't loaded yet
+            if (!isSet($this->loadedWidgets[$directory_name])) {
+                $file = $oWebsite->getUriWidgets() . $directory_name . "/main.php";
+                if (file_exists($file)) {
+                    require($file);
+                } else {
+                    $oWebsite->getText()->logError("The widget $directory_name (id=$id) was not found. File <code>$file</code> was missing.");
+                    $oWebsite->addError("A widget was missing.");
+                    continue;
+                }
+            }
+
+            // Check if load was succesfull. Display widget or display error.
+            if (isSet($this->loadedWidgets[$directory_name])) {
+                $output.= $this->loadedWidgets[$directory_name]->getText($this->websiteObject, $id, $widget->getData());
+                if ($loggedInAsAdmin) {
+                    // Links for editing and deleting
+                    $output.= "<p>\n";
+                    $output.= '<a class="arrow" href="' . $oWebsite->getUrlPage("edit_widget", $id) . '">' . $oWebsite->t("main.edit") . " " . $oWebsite->t("main.widget") . '</a> ';
+                    $output.= '<a class="arrow" href="' . $oWebsite->getUrlPage("delete_widget", $id) . '">' . $oWebsite->t("main.delete") . " " . $oWebsite->t("main.widget") . '</a> ';
+                    $output.= "</p>\n";
+                }
+            } else {
+                $oWebsite->getText()->logError("The widget $directory_name (id=$id) could not be loaded. File <code>$file</code> is incorrect.");
+                $oWebsite->addError("A widget was missing.");
+            }
+        }
+
+        // Link to manage widgets
+        if ($loggedInAsAdmin) {
+            $output.= '<p><a class="arrow" href="' . $oWebsite->getUrlPage("widgets") . '">';
+            $output.= $oWebsite->t("main.manage") . " " . strToLower($oWebsite->t("main.widgets"));
+            $output.= "</a></p>\n";
+        }
+
+        return $output;
+    }
+
+    private function getSiteInstallError($sidebarId) {
+        $website = $this->websiteObject;
+        if ($sidebarId != 1) {
+            return "";
+        }
+        if (!$website->getDatabase()->isInstalled()) {
+            return <<<ERROR
+                <p>{$website->t("errors.site.not_installed")}</p>
+                <p>
+                    <a class="button primary_button" href="{$website->getUrlPage("installing_database")}">
+                        {$website->t("errors.site.install")}
+                    </a>
+                </p>
+ERROR;
+        }
+        return <<<ERROR
+            <p>{$website->t("errors.site.not_updated")}</p>
+            <p>
+                <a class="button primary_button" href="{$website->getUrlPage("installing_database")}">
+                    {$website->t("errors.site.update")}
+                </a>
+            </p>
+ERROR;
+    }
+
+    /**
+     * Registers a widget. Widget files should call this.
+     * @param WidgetDefinition $widget The widget to register.
+     */
+    public function registerWidget(WidgetDefinition $widget) {
+        $this->loadedWidgets[$this->widgetDirectoryName] = $widget;
+    }
+
+}

src/Page/delete_widget.inc
@@ -14,7 +14,7 @@ if ($loggedIn) {
     $widget_id = isSet($_REQUEST['id']) ? (int) $_REQUEST['id'] : 0;
     $widget = null;
     if ($widget_id > 0) {
-        $oWidgets = new Widgets($this);
+        $oWidgets = new WidgetRepository($this);
         $widget = $oWidgets->getPlacedWidget($widget_id);
         unset($oWidgets);
     }

src/Page/edit_widget.inc
@@ -7,7 +7,7 @@ $oAuth = new Authentication($this);
 $loggedIn = $oAuth->check(Authentication::$ADMIN_RANK, true);
 
 // CONTENTS
-$oWidgets = new Widgets($this);
+$oWidgets = new WidgetRepository($this);
 $textToDisplay = '';
 
 if ($loggedIn) {

commit 7956781e1e714cdc773107e56c703a400ca5abc1

Fix widget editing being broken by 774a629857888d8cd056949c30185b7eb17f4816

content/widgets/links/main.php
@@ -3,6 +3,7 @@
 namespace Rcms\Extend\Widget;
 
 use Rcms\Core\LinkRepository;
+use Rcms\Core\MenuRepository;
 use Rcms\Core\Website;
 use Rcms\Core\WidgetDefinition;
 
@@ -50,17 +51,17 @@ class WidgetRkokLinks extends WidgetDefinition {
         $returnValue = "";
         $title_max_length = self::TITLE_MAX_LENGTH; // Herodoc doesn't support constants
         // Build menu options
-        $oMenu = new LinkRepository($oWebsite);
-        $menus = $oMenu->getMenus();
+        $oMenu = new MenuRepository($oWebsite->getDatabase());
+        $menus = $oMenu->getAllMenus();
         $menu_options = "";
         if (count($menus) > 0) {
             $menu_options.= "<select name=\"menu_id_$id\" id=\"menu_id_$id\">\n";
-            foreach ($menus as $available_menu_id => $menu_name) {
-                $menu_options.= '<option value="' . $available_menu_id . '"';
-                if ($available_menu_id == $menu_id) {
+            foreach ($menus as $menu) {
+                $menu_options.= '<option value="' . $menu->getId() . '"';
+                if ($menu->getId() == $menu_id) {
                     $menu_options.= ' selected="selected"';
                 }
-                $menu_options.= '>' . $menu_name . "</option>\n";
+                $menu_options.= '>' . htmlSpecialChars($menu->getName()) . "</option>\n";
             }
             $menu_options.="</select>\n";
         } else {

content/widgets/text/main.php
@@ -2,6 +2,7 @@
 
 namespace Rcms\Extend\Widget;
 
+use Rcms\Core\Editor;
 use Rcms\Core\Website;
 use Rcms\Core\WidgetDefinition;
 

src/Core/PlacedWidget.php
@@ -2,14 +2,21 @@
 
 namespace Rcms\Core;
 
+use InvalidArgumentException;
+
 use Rcms\Core\Repository\Entity;
 
+/**
+ * Represents a widget that has been placed somewhere. It consists of the
+ * {@link WidgetDefinition} that is used, as well as the settings for the widget.
+ * Together this information can be used to render the widget.
+ */
 class PlacedWidget extends Entity {
 
     protected $id;
     protected $sidebarId;
-    protected $widgetData;
-    protected $priority;
+    protected $widgetData = array();
+    protected $priority = 0;
     protected $widgetName;
     protected $baseDirectory;
 
@@ -24,8 +31,23 @@ class PlacedWidget extends Entity {
         $this->baseDirectory = $baseDirectory;
     }
 
+    /**
+     * Creates a new placed widget. Won't be saved automatically, save it to a
+     * widget repository.
+     * @param string $baseDirectory Base directory of all widgets.
+     * @param string $widgetName Name of the widget.
+     * @param int $sidebarId Id of the sidebar the widget is placed in.
+     * @return PlacedWidget The placed widget.
+     */
+    public static function newPlacedWidget($baseDirectory, $widgetName, $sidebarId) {
+        $placedWidget = new PlacedWidget($baseDirectory);
+        $placedWidget->setSidebarId($sidebarId);
+        $placedWidget->widgetName = (string) $widgetName;
+        return $placedWidget;
+    }
+
     public function getWidgetDefinition(WidgetRepository $widget_loader) {
-        return $widget_loader->getWidgetDefinition($this->baseDirectory . $this->widgetName . '/');
+        return $widget_loader->getWidgetDefinition($this->widgetName);
     }
 
     /**
@@ -40,15 +62,17 @@ class PlacedWidget extends Entity {
     /**
      * Sets the internal data array to the new value. Can be null. Silently
      * fails if the data is invalidated by setting $data["valid"] to false.
-     * @param mixed $data The new data.
+     * @param array|null $data The new data.
      */
     public function setData($data) {
         if ($data == null) {
             $this->widgetData = array();
-        } else {
+        } else if (is_array($data)) {
             if (!isSet($data["valid"]) || $data["valid"]) {
                 $this->widgetData = $data;
             }
+        } else {
+            throw new InvalidArgumentException("data must be array or null, $data given");
         }
     }
 
@@ -91,56 +115,6 @@ class PlacedWidget extends Entity {
      */
     public function getDirectoryName() {
         return $this->widgetName;
-    }
-
-    /**
-     * Saves all changes.
-     * @param Database $oDatabase The database to save to.
-     * @return boolean Whether the save was successfull. On failure, an error
-     *     message is printed automatically.
-     */
-    public function save(Database $oDatabase) {
-        if ($this->id > 0) {
-            // Update
-            $sql = "UPDATE `widgets` SET ";
-            $sql.= '`widget_data` = "' . $oDatabase->escapeData($this->dataString) . '", ';
-            $sql.= '`sidebar_id` = ' . $this->sidebarId . ', ';
-            $sql.= '`widget_priority` = ' . $this->priority . ' ';
-            $sql.= "WHERE `widget_id` = " . $this->id;
-            if ($oDatabase->query($sql)) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            // Add
-            $sql = "INSERT INTO `widgets` (`widget_naam`, `widget_data`, ";
-            $sql.= "`sidebar_id`, `widget_priority`) VALUES (";
-            $sql.= '"' . $oDatabase->escapeData($this->directoryName) . '", ';
-            $sql.= '"' . $oDatabase->escapeData($this->dataString) . '", ';
-            $sql.= $this->sidebarId . ', ';
-            $sql.= $this->priority . ')';
-            if ($oDatabase->query($sql)) {
-                $this->id = $oDatabase->lastInsertId();
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-
-    /**
-     * Deletes this widget from the database.
-     * @param Database $oDatabase The database to delete from.
-     * @return boolean Whether the deletion was successfull.
-     */
-    public function delete(Database $oDatabase) {
-        if ($oDatabase->query("DELETE FROM `widgets` WHERE `widget_id`= " . $this->id)) {
-            $this->id = 0; // Reset
-            return true;
-        } else {
-            return false;
-        }
     }
 
 }

src/Core/WidgetRepository.php
@@ -2,6 +2,8 @@
 
 namespace Rcms\Core;
 
+use PDOException;
+
 use Rcms\Core\Exception\NotFoundException;
 use Rcms\Core\Repository\Field;
 use Rcms\Core\Repository\Repository;
@@ -115,7 +117,7 @@ class WidgetRepository extends Repository {
     }
 
     /**
-     * Returns the widget the give directory.
+     * Returns the widget in the given directory.
      * @param string $widgetDirectoryName Directory name. Do not include the full path.
      * @return WidgetDefinition The widget, or null if not found.
      */
@@ -221,6 +223,26 @@ ERROR;
      */
     public function registerWidget(WidgetDefinition $widget) {
         $this->loadedWidgets[$this->widgetDirectoryName] = $widget;
+    }
+
+    /**
+     * Saves the given placed widget to the database.
+     * @param PlacedWidget $placedWidget The placed widget.
+     * @throws PDOException If saving fails.
+     */
+    public function savePlacedWidget(PlacedWidget $placedWidget) {
+        $this->saveEntity($placedWidget);
+    }
+    
+    /**
+     * Deletes the placed widget from the database.
+     * @param PlacedWidget $placedWidget The placed widget.
+     * @throws NotFoundException If the placed widget doesn't appear in the
+     * database.
+     * @throws PDOException If a database error occurs.
+     */
+    public function deletePlacedWidget(PlacedWidget $placedWidget) {
+        $this->where($this->widgetIdField, '=', $placedWidget->getId())->deleteOneOrFail();
     }
 
 }

src/Page/delete_widget.inc
@@ -13,21 +13,16 @@ $textToDisplay = '';
 if ($loggedIn) {
     $widget_id = isSet($_REQUEST['id']) ? (int) $_REQUEST['id'] : 0;
     $widget = null;
+    $oWidgets = new WidgetRepository($this);
     if ($widget_id > 0) {
-        $oWidgets = new WidgetRepository($this);
         $widget = $oWidgets->getPlacedWidget($widget_id);
-        unset($oWidgets);
     }
-    if ($widget) {
+    if ($widget !== null) {
         if (isSet($_REQUEST['confirm'])) {
             // Confirmed
-            if ($widget->delete($this->getDatabase())) {
-                $textToDisplay.= '<p>' . $this->t("main.widget") . ' ' . $this->t("editor.is_deleted") . '</p>';
-                $textToDisplay.= '<p><a class="arrow" href="' . $this->getUrlPage("widgets") . '">' . $this->t("main.ok") . '</a></p>';
-            } else {
-                $textToDisplay.= '<p>' . $this->t("main.widget") . ' ' . $this->t("errors.is_not_removed") . '</p>';
-                $textToDisplay.= '<p><a class="arrow" href="' . $this->getUrlPage("widgets") . '">' . $this->t("main.ok") . '</a></p>';
-            }
+            $oWidgets->deletePlacedWidget($widget);
+            $textToDisplay.= '<p>' . $this->t("main.widget") . ' ' . $this->t("editor.is_deleted") . '</p>';
+            $textToDisplay.= '<p><a class="arrow" href="' . $this->getUrlPage("widgets") . '">' . $this->t("main.ok") . '</a></p>';
         } else {
             // Show confirmation message
             $textToDisplay.= '<p>' . $this->t("editor.widget.delete.confirm") . '</p>';

src/Page/edit_widget.inc
@@ -21,14 +21,14 @@ if ($loggedIn) {
             $directory_name = isSet($_REQUEST['directory_name']) ? $_REQUEST['directory_name'] : "";
             $directory = $this->getUriWidgets() . '/' . $_REQUEST['directory_name'];
             if ($directory_name != "" && is_dir($directory)) {
-                $widget = new PlacedWidget(0, $sidebar_id, $directory_name, "", 0, $directory);
+                $widget = PlacedWidget::newPlacedWidget($directory, $directory_name, $sidebar_id);
             }
         }
     } elseif ($widget_id > 0) {
         // Existing widget
         $widget = $oWidgets->getPlacedWidget($widget_id);
     }
-    if ($widget == null) {
+    if ($widget === null) {
         // Tried to edit non-existing widget or tried to create invalid widget
         // Show meaningless error
         $this->addError($this->t("main.widget") . ' ' . $this->t("errors.not_found"));
@@ -67,21 +67,17 @@ if ($loggedIn) {
                 // Get whether this is a new widget (id will be changed to a real
                 // one after it has saved, so check has to come before that)
                 $is_new_widget = ($widget->getId() == 0);
-                if ($widget->save($this->getDatabase())) {
-                    if ($is_new_widget) {
-                        // Created
-                        $textToDisplay.= "<p><em>" . $this->t("main.widget") . " " . $this->t("editor.is_created") . "</em>\n";
-                        $textToDisplay.= '<a class="arrow" href="' . $this->getUrlPage("widgets") . '">' . $this->t("main.widgets") . "</a>\n";
-                        $textToDisplay.= '<a class="arrow" href="' . $this->getUrlPage("home") . '">' . $this->t("main.home") . "</a></p>\n";
-                    } else {
-                        // Updated
-                        $textToDisplay.= "<p><em>" . $this->t("main.widget") . " " . $this->t("editor.is_edited") . "</em>\n";
-                        $textToDisplay.= '<a class="arrow" href="' . $this->getUrlPage("widgets") . '">' . $this->t("main.widgets") . "</a>\n";
-                        $textToDisplay.= '<a class="arrow" href="' . $this->getUrlPage("home") . '">' . $this->t("main.home") . "</a></p>\n";
-                    }
+                $oWidgets->savePlacedWidget($widget);
+                if ($is_new_widget) {
+                    // Created
+                    $textToDisplay.= "<p><em>" . $this->t("main.widget") . " " . $this->t("editor.is_created") . "</em>\n";
+                    $textToDisplay.= '<a class="arrow" href="' . $this->getUrlPage("widgets") . '">' . $this->t("main.widgets") . "</a>\n";
+                    $textToDisplay.= '<a class="arrow" href="' . $this->getUrlPage("home") . '">' . $this->t("main.home") . "</a></p>\n";
                 } else {
-                    // Database error, not saved
-                    $textToDisplay.= "<p><em>" . $this->t("main.widget") . " " . $this->t("errors.not_saved") . "</em></p>\n";
+                    // Updated
+                    $textToDisplay.= "<p><em>" . $this->t("main.widget") . " " . $this->t("editor.is_edited") . "</em>\n";
+                    $textToDisplay.= '<a class="arrow" href="' . $this->getUrlPage("widgets") . '">' . $this->t("main.widgets") . "</a>\n";
+                    $textToDisplay.= '<a class="arrow" href="' . $this->getUrlPage("home") . '">' . $this->t("main.home") . "</a></p>\n";
                 }
             } else {
                 // User error, not saved