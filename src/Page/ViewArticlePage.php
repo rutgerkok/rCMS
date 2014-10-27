d682b17e71f96b2f5dbb1cfde19776c9b6b65fc6
rutgerkok <rutger.kok@live.com>
2014-10-27 20:17:09 +0100

    Redirect page view_article to article
    As of 972e4274e801608fd61ab87b52a8a8606592bdf4 pages can now easily be
    redirected.

--- src/Page/ViewArticlePage.php
+++ src/Page/ViewArticlePage.php
@@ -2,10 +2,31 @@
 
 namespace Rcms\Page;
 
+use Rcms\Core\Exception\RedirectException;
+use Rcms\Core\Text;
+use Rcms\Core\Request;
+use Rcms\Page\View\EmptyView;
+
 /**
  * This class exists to redirect any old view_article
  * urls to the newer article urls.
  */
-class ViewArticlePage extends ArticlePage {
-    
+class ViewArticlePage extends Page {
+
+    public function init(Request $request) {
+        $website = $request->getWebsite();
+        $id = $request->getParamInt(0, 0);
+        $rawUrl = urldecode($website->getUrlPage("article", $id));
+
+        throw new RedirectException($rawUrl, RedirectException::TYPE_ALWAYS);
+    }
+
+    public function getPageTitle(Text $text) {
+        return "";
+    }
+
+    public function getView(Text $text) {
+        return new EmptyView($text);
+    }
+
 }
