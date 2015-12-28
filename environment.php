e5582fbb2581634abc29dfff1f79e5faa7a24619
rutgerkok <rutger.kok@live.com>
2015-12-28 13:41:24 +0100

    Add composer.json, use the autoloader of Composer

--- environment.php
+++ environment.php
@@ -4,14 +4,7 @@
 // basic changes to the environment, like setting an autoloader.
 
 // Classloader
-spl_autoload_register(function($fullClassName) {
-    $class = str_replace('\\', '/', subStr($fullClassName, strLen("Rcms\\")));
-
-    // Try to see if it's a class in the library
-    if (file_exists('src/' . $class . '.php')) {
-        require_once('src/' . $class . '.php');
-    }
-});
+require __DIR__ . '/vendor/autoload.php';
 
 // Report all errors
 error_reporting(E_ALL);
